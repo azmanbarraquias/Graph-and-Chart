adf8fc367e5e5e5c7cec7f4ba3d0882ca96d446e
Azman Barraquias <azmanbarraquias@gmail.com>
2020-07-15 19:05:37 +0800

    Commit project to repository

---
+++ Assets/Pie Chart/PieGraphManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PieGraphManager : MonoBehaviour
+{
+    [Header("Setup")]
+    public Image wedgePrefabs;
+
+    [Space]
+    public Wedge[] wedges;
+
+    private void Start()
+    {
+        ClearAll();
+        CreateGraph();
+    }
+
+    public void CreateGraph()
+    {
+        float total = wedges.Sum(w => w.value);
+        float zRotation = 0;
+
+        foreach (Wedge wedge in wedges)
+        {
+            Image newWedge = Instantiate(wedgePrefabs);
+
+            newWedge.name = wedge.title;
+
+            Transform child = newWedge.transform.GetChild(0);
+
+            newWedge.transform.SetParent(transform, false);
+
+            child.GetComponent<TextMeshProUGUI>().text = wedge.title;
+
+            newWedge.color = wedge.color;
+
+            newWedge.fillAmount = wedge.value / total;
+
+            newWedge.transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRotation));
+
+            child.rotation = Quaternion.Euler(0.0f, 0.0f, newWedge.transform.rotation.z);
+
+            zRotation -= newWedge.fillAmount * 360f;
+        }
+    }
+
+    public void ClearAll()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+}
+
+
---
+++ Assets/Pie Chart/Wedge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wedge
+{
+    public string title;
+
+    [Range(1f, 100f)]
+    public float value;
+
+    public Color color;
+
+}
